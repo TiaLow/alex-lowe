cf8b135df57431ee4e6a6f526f36e13c716ef478
Tia Low <lowtia@gmail.com>
2026-07-21 13:47:23 -0700

    Update research page styling

--- src/pages/Research/Research.tsx
+++ src/pages/Research/Research.tsx
@@ -1,136 +1,60 @@
 import { Container, Typography, Box, Divider } from "@mui/material";
-import ResearchGrid from "./components/ResearchGrid";
 import fossil1 from "../../assets/images/fossil1.jpg";
 import ResearchSection from "./components/ResearchSection";
-
-const researchItems1 = [
-  {
-    title: "Relevant publications from my PhD dissertation",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-  {
-    title: "Relevant publications from my PhD dissertation",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-  {
-    title: "Relevant publications from my PhD dissertation",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-  {
-    title: "Relevant publications from my PhD dissertation",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-];
-
-const researchItems2 = [
-  {
-    title: "collawash field and fossil picture",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-  {
-    title: "Lorem Ipsum Dolor Sit Amet",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-];
-
-const researchItems3 = [
-  {
-    title: "ash + leaf, and zircon image",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-];
-
-const researchItems4 = [
-  {
-    title: "slingshot image",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-];
-
-const researchItems5 = [
-  {
-    title: "field work",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-  {
-    title: "field work",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. ",
-    image: fossil1,
-  },
-];
 
 export default function Research() {
   return (
     <Container maxWidth="lg" sx={{ py: { xs: 4, md: 8 } }}>
-      {/* HEADER */}
       <Box sx={{ mb: { xs: 4, md: 8 } }}>
         <Typography variant="h3" sx={{ fontWeight: 800 }}>
           Research
-        </Typography>
-
-        <Typography variant="body1" color="text.secondary" sx={{ mt: 2 }}>
-          Descriptive overview here (A selection of ongoing and past research
-          projects focused on paleobotany, climate reconstruction, and
-          evolutionary plant science.)
         </Typography>
       </Box>
 
       <Divider sx={{ mb: { xs: 4, md: 6 } }} />
 
-      {/* GRID */}
       <ResearchSection
         title="Plant community and climates across Miocene in PNW"
         description="The Pacific Northwest (PNW), including the states of WA, OR, and ID, is a Cenozoic paleobotanical treasure trove! In particular, Oligocene to Miocene sites abound and showcase deciduous dominated mixed temperate forests in the midst of global climatic changes and pervasive regional volcanism. For example, a peak period of warmth in the mid Miocene (Miocene Climatic Optimum; MCO) likely represents the most recent time atmospheric CO2 reached levels we expect for our near future. My research reconstructs patterns of regional vegetation and climate across this critical time period, following up on work from the early to mid 1900’s that has seen little re-investigation. While my expertise is in plant macrofossils, I collaborate closely with palynologists and phytolith experts to paint a more comprehensive regional picture of vegetation and climate."
-      >
-        <ResearchGrid items={researchItems1} />
-      </ResearchSection>
+        bullets={["Link", "Link", "Link"]}
+        image={fossil1}
+        imageAlt="Fossil image"
+      />
 
       <ResearchSection
-        title=""
+        title="Title"
         description="Early Miocene floras, representing pre-Miocene Climatic Optimum (MCO) environments, are much more rare in the Pacific Northwest than those from the mid Miocene, representing the height of the MCO. I am currently re-investigating the composition of the early Miocene Collawash and Eagle Creek floras to help fill this time gap and provide a pre-warming baseline to compare MCO floras to."
-      >
-        <ResearchGrid items={researchItems2} />
-      </ResearchSection>
+        bullets={["Link", "Link", "Link"]}
+        image={fossil1}
+        imageAlt="Fossil image"
+        reverse
+      />
 
       <ResearchSection
         title="Geochronology"
         description="Knowing about time is important for plant sites. Much of the current knowledge is limited. I address it like this."
-      >
-        <ResearchGrid items={researchItems3} />
-      </ResearchSection>
+        bullets={["Link", "Link", "Link"]}
+        image={fossil1}
+        imageAlt="Fossil image"
+      />
 
       <ResearchSection
         title="Paleoecology and paleoclimate proxy development and refinement"
         description="Description"
-      >
-        <ResearchGrid items={researchItems4} />
-      </ResearchSection>
+        bullets={["Link", "Link", "Link"]}
+        image={fossil1}
+        imageAlt="Fossil image"
+        reverse
+      />
 
       <ResearchSection
         title="Eocene climate and the Okanagan Highlands floras"
         description="Description"
-      >
-        <ResearchGrid items={researchItems5} />
-      </ResearchSection>
+        bullets={["Link", "Link", "Link"]}
+        image={fossil1}
+        imageAlt="Fossil image"
+        showDivider={false}
+      />
     </Container>
   );
 }
--- src/pages/Research/components/ResearchSection.tsx
+++ src/pages/Research/components/ResearchSection.tsx
@@ -1,39 +1,124 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 
 type ResearchSectionProps = {
   title: string;
   description: string;
-  children: React.ReactNode;
+  bullets: string[];
+  image: string;
+  imageAlt: string;
+  reverse?: boolean;
+  showDivider?: boolean;
 };
 
 export default function ResearchSection({
   title,
   description,
-  children,
+  bullets,
+  image,
+  imageAlt,
+  reverse = false,
+  showDivider = true,
 }: ResearchSectionProps) {
   return (
-    <Box sx={{ mb: { xs: 8, md: 12 } }}>
-      <Typography
-        variant="h4"
+    <>
+      <Box
+        component="section"
         sx={{
-          fontWeight: 700,
-          mb: 2,
+          py: { xs: 5, md: 4 },
         }}
       >
-        {title}
-      </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column",
+              md: reverse ? "row-reverse" : "row",
+            },
+            alignItems: {
+              xs: "center",
+              md: "flex-start",
+            },
+            gap: 6,
+          }}
+        >
+          {/* Text */}
+          <Box
+            sx={{
+              flex: 1,
+            }}
+          >
+            <Typography
+              variant="h5"
+              sx={{
+                fontWeight: 700,
+                mb: 2,
+              }}
+            >
+              {title}
+            </Typography>
 
-      <Typography
-        color="text.secondary"
-        sx={{
-          maxWidth: 900,
-          mb: 4,
-        }}
-      >
-        {description}
-      </Typography>
+            <Typography sx={{ mb: 2 }}>{description}</Typography>
 
-      {children}
-    </Box>
+            <List
+              disablePadding
+              sx={{
+                listStyleType: "disc",
+                pl: 3,
+              }}
+            >
+              {bullets.map((bullet) => (
+                <ListItem
+                  key={bullet}
+                  disablePadding
+                  sx={{
+                    display: "list-item",
+                    mb: 0.5,
+                  }}
+                >
+                  <ListItemText primary={bullet} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+
+          {/* Image */}
+          <Box
+            sx={{
+              flex: 1,
+            }}
+          >
+            <Box
+              component="img"
+              src={image}
+              alt={imageAlt}
+              sx={{
+                display: "block",
+                width: "100%",
+                maxWidth: 500,
+                ml: reverse ? 0 : "auto",
+                borderRadius: 3,
+                boxShadow: 4,
+                objectFit: "cover",
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+
+      {showDivider && (
+        <Divider
+          sx={{
+            borderColor: "grey.300",
+          }}
+        />
+      )}
+    </>
   );
 }
